8a96a02356b4b28ebfb8753720aa1f65408bb9d2
Agustin_Ibarra <139735608+AgustinIbarra2004@users.noreply.github.com>
2024-06-01 23:39:47 -0300

    To Do list the Javascript
    Sigue confiando papu :D

---
+++ Practica-2.js
@@ -0,0 +1,40 @@
+function agregarTarea() {
+    // Obtenemos el valor de la tarea
+    let valorTarea = document.getElementById("nuevaTarea").value;
+
+    // Validamos que el valor no sea vacío
+    if (valorTarea === "") {
+        return alert("Escribe algo para agregar a la lista");
+    }
+
+    // Crear elemento de la lista
+    let elementoLista = document.createElement("li");
+
+    // Crear checkbox
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+
+    // Agregar checkbox al elemento de la lista
+    elementoLista.appendChild(checkbox);
+
+    // Agregar texto al elemento de la lista
+    let textoTarea = document.createTextNode(" " + valorTarea + " ");
+    elementoLista.appendChild(textoTarea);
+
+    // Crear botón de eliminar
+    let botonEliminar = document.createElement("button");
+    botonEliminar.textContent = "Eliminar";
+    // La función que tiene el botón para eliminar el elemento de la lista
+    botonEliminar.onclick = function() {
+        elementoLista.remove();
+    };
+    // Agregar botón de eliminar al elemento de la lista
+    elementoLista.appendChild(botonEliminar);
+
+    // Seleccionar la lista y agregar el elemento de la lista
+    let listaTarea = document.getElementById("listaTarea");
+    listaTarea.appendChild(elementoLista);
+
+    // Limpiar el cuadro de texto de nombre de la tarea
+    document.getElementById("nuevaTarea").value = "";
+}
